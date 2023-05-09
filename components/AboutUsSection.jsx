accf4f203b39cca531d1b3b6b07a117dc4d5c838
D Dheeraj <dhiraajroy123@gmail.com>
2023-05-09 22:37:59 +0530

    updated posts and about us

--- components/AboutUsSection.jsx
+++ components/AboutUsSection.jsx
@@ -42,6 +42,10 @@ export default function AboutUsSection() {
             Developed By D Dheeraj<br />
             E-Mail: <Link className='underline underline-offset-4' href="mailto:dhiraajroy123@gmail.com">dhiraajroy123@gmail.com</Link>
           </div>
+          <div className='mt-5 font-sans [&>*]:font-sans'>
+            Managed By Sarim Haque<br />
+            {/* E-Mail: <Link className='underline underline-offset-4' href="mailto:dhiraajroy123@gmail.com">dhiraajroy123@gmail.com</Link> */}
+          </div>
         </div>
       </div>
     </Section>
