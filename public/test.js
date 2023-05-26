6b3b7c82355a1d210c029f5064c116b3a3d0ff14
D Dheeraj <dhiraajroy123@gmail.com>
2023-05-26 16:45:05 +0530

    updated events

--- public/test.js
+++ public/test.js
@@ -4,7 +4,7 @@ fs.readFile('events.json', (err, data) => {
   if (err) throw err;
   
   const eventsData = JSON.parse(data);
-  const filteredData = eventsData.filter(event => event.Category !== "");
+  const filteredData = eventsData.filter(event => event.field1 !== "");
   const jsonData = JSON.stringify(filteredData);
 
   fs.writeFile('finalEvents.json', jsonData, (err) => {
