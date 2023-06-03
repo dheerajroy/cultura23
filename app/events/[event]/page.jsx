94de3ec218fa146082e508a769034dc950486cae
D Dheeraj <dhiraajroy123@gmail.com>
2023-06-03 21:25:56 +0530

    added disable feature for events

--- app/events/[event]/page.jsx
+++ app/events/[event]/page.jsx
@@ -8,11 +8,16 @@ import events from '@/public/finalEvents.json'
 export default function Event({ params }) {
     const event = events.Events.filter(event => event['Event Name'].toLowerCase().trim().replace(/\s+/g, '-') === params.event)[0]
     const img = require(`/public/images/posters/${event['Event Name'].trim()}.png`)
-
+    const deletedEvents = ['BATTLE OF BANDS']
+    let disableBtn = false
+    for (let i = 0; i < deletedEvents.length; i++)
+        if (deletedEvents[i] === event['Event Name']) {
+            disableBtn = true
+            break
+        }
     return (
         <Section>
             <div className="flex flex-wrap justify-center gap-5 mt-20">
-                {/* <div className="lg:sticky top-[70px] w-[300px] h-[300px] md:w-[500px] lg:h-[500px] bg-neutral-500"></div> */}
                 <div>
                     <Image src={`/images/posters/${event['Event Name'].trim()}.png`} width={300} height={300} className="lg:sticky top-[70px] w-[300px] h-[300px] md:w-[500px] lg:h-[500px] bg-neutral-500 rounded-xl" />
                 </div>
@@ -23,9 +28,11 @@ export default function Event({ params }) {
                         {event['Rules and Regulations'] ? <h1 className="font-LUSER text-3xl">Rules and Regulations</h1> : ""}
                         <p className="font-sans" dangerouslySetInnerHTML={{ __html: event['Rules and Regulations'].replace(/\n/g, '<br /><br />') }} />
                     </div>
-                    <Link href={`${event['Reg link']}`} target="_blank">
-                        <Button className="text-3xl">Register</Button>
-                    </Link>
+                    {disableBtn ? "" :
+                        <Link href={`${event['Reg link']}`} target="_blank">
+                            <Button className="text-3xl">Register</Button>
+                        </Link>
+                    }
                 </div>
             </div>
         </Section>
--- components/EventCard.jsx
+++ components/EventCard.jsx
@@ -5,8 +5,8 @@ export default function EventCard({ title, clar, description, slug }) {
     return (
         <div className="bg-black/50 backdrop-blur-lg p-5 rounded-xl flex flex-col justify-between gap-5 font-SivarPro w-[300px]">
             <div>
-            <h1 className="text-3xl font-LUSER tracking-widest">{title}</h1>
-            {clar ? <h2 className="text-lg">({clar})</h2> : ""}
+                <h1 className="text-3xl font-LUSER tracking-widest">{title}</h1>
+                {clar ? <h2 className="text-lg">({clar})</h2> : ""}
             </div>
             <p className="text-lg">{description}</p>
             <Link href={`/events/${slug}`}>
